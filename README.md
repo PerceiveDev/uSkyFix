5a0034cbfb1d058d812aee72016764390796357c
Rayzr522 <rayzr522@gmail.com>
2016-09-26 15:01:08 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # uSkyFix
 A PlaceholderAPI fix for uSkyBlock
+
+### Building
+This requires both maven and a `uSkyBlock.jar` file. To build, simply place your `uSkyBlock.jar` file in the `lib` folder, and the run `mvn clean package` in the command line or in Eclipse run `Maven build...` with the goals of `clean package`.
---
+++ src/main/java/com/perceivedev/uskyfix/uSkyFix.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.perceivedev.uskyfix;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.clip.placeholderapi.external.EZPlaceholderHook;
+import us.talabrek.ultimateskyblock.uSkyBlock;
+
+/**
+ * @author Rayzr
+ *
+ */
+public class uSkyFix extends JavaPlugin {
+
+    private Logger            logger;
+    private EZPlaceholderHook hook;
+
+    @Override
+    public void onEnable() {
+        logger = getLogger();
+
+        if (Bukkit.getPluginManager().getPlugin("uSkyBlock") == null || Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            logger.severe("This pugin requres both PlaceholerAPI and uSkyBlock!");
+            logger.severe("Please download them before attempting to use this.");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        hook = new EZPlaceholderHook(this, "uskyfix") {
+
+            @Override
+            public String onPlaceholderRequest(Player p, String identifier) {
+                if (p == null) {
+                    return "";
+                }
+                if (identifier.equals("island_level")) {
+                    return String.format("%.2f", uSkyBlock.getAPI().getIslandLevel(p));
+                }
+                return null;
+            }
+
+        };
+        hook.hook();
+
+        logger.info(versionText() + " enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        logger.info(versionText() + " disabled");
+    }
+
+    public String versionText() {
+        return getName() + " v" + getDescription().getVersion();
+    }
+
+}
