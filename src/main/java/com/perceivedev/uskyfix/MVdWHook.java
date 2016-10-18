b4178406b1269af15555448c379ff4837c10a593
Rayzr522 <rayzr522@gmail.com>
2016-10-18 07:37:55 -0400

    Added a bunch o' new placeholders

--- src/main/java/com/perceivedev/uskyfix/MVdWHook.java
+++ src/main/java/com/perceivedev/uskyfix/MVdWHook.java
@@ -3,6 +3,8 @@
  */
 package com.perceivedev.uskyfix;
 
+import java.lang.reflect.Method;
+
 import org.bukkit.plugin.Plugin;
 
 import be.maximvdw.placeholderapi.PlaceholderAPI;
@@ -16,6 +18,8 @@ import us.talabrek.ultimateskyblock.api.IslandInfo;
  *
  */
 public class MVdWHook {
+
+    private static Method IS_LOCKED;
 
     public static void register(Plugin plugin) {
         PlaceholderAPI.registerPlaceholder(plugin, "uskyfix_island_level", new PlaceholderReplacer() {
@@ -35,6 +39,42 @@ public class MVdWHook {
                 return info == null ? "None" : info.getLeader();
             }
         });
+        PlaceholderAPI.registerPlaceholder(plugin, "uskyfix_island_biome", new PlaceholderReplacer() {
+            public String onPlaceholderReplace(PlaceholderReplaceEvent e) {
+                IslandInfo info = uSkyBlock.getAPI().getIslandInfo(e.getPlayer());
+                return info == null ? "None" : info.getBiome();
+            }
+        });
+        PlaceholderAPI.registerPlaceholder(plugin, "uskyfix_island_members", new PlaceholderReplacer() {
+            public String onPlaceholderReplace(PlaceholderReplaceEvent e) {
+                IslandInfo info = uSkyBlock.getAPI().getIslandInfo(e.getPlayer());
+                return (info == null || info.getMembers().size() < 1) ? "0" : "" + info.getMembers().size();
+            }
+        });
+        PlaceholderAPI.registerPlaceholder(plugin, "uskyfix_island_togglewarp", new PlaceholderReplacer() {
+            public String onPlaceholderReplace(PlaceholderReplaceEvent e) {
+                IslandInfo info = uSkyBlock.getAPI().getIslandInfo(e.getPlayer());
+                return info == null ? "None" : (info.getWarpLocation() == null ? "Inactive" : "Active");
+            }
+        });
+        PlaceholderAPI.registerPlaceholder(plugin, "uskyfix_island_lock", new PlaceholderReplacer() {
+            public String onPlaceholderReplace(PlaceholderReplaceEvent e) {
+                IslandInfo info = uSkyBlock.getAPI().getIslandInfo(e.getPlayer());
+                return info == null ? "None" : isLocked(info) ? "Locked" : "Unlocked";
+            }
+        });
+    }
+
+    public static boolean isLocked(IslandInfo info) {
+        try {
+            if (IS_LOCKED == null) {
+                IS_LOCKED = info.getClass().getMethod("isLocked");
+            }
+            return Boolean.valueOf(IS_LOCKED.invoke(info).toString()).booleanValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
--- src/main/java/com/perceivedev/uskyfix/PAPIHook.java
+++ src/main/java/com/perceivedev/uskyfix/PAPIHook.java
@@ -3,6 +3,8 @@
  */
 package com.perceivedev.uskyfix;
 
+import java.lang.reflect.Method;
+
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -16,12 +18,14 @@ import us.talabrek.ultimateskyblock.api.IslandInfo;
  */
 public class PAPIHook {
 
+    private static Method IS_LOCKED;
+
     public static void register(Plugin plugin) {
         new EZPlaceholderHook(plugin, "uskyfix") {
 
             @Override
             public String onPlaceholderRequest(Player p, String identifier) {
-                if (p == null) {
+                if (p == null || !p.isOnline()) {
                     return "";
                 }
                 if (identifier.equals("island_level")) {
@@ -32,11 +36,35 @@ public class PAPIHook {
                 } else if (identifier.equals("island_leader")) {
                     IslandInfo info = uSkyBlock.getAPI().getIslandInfo(p.getLocation());
                     return info == null ? "None" : info.getLeader();
+                } else if (identifier.equals("island_biome")) {
+                    IslandInfo info = uSkyBlock.getAPI().getIslandInfo(p);
+                    return info == null ? "None" : info.getBiome();
+                } else if (identifier.equals("island_members")) {
+                    IslandInfo info = uSkyBlock.getAPI().getIslandInfo(p);
+                    return (info == null || info.getMembers().size() < 1) ? "0" : "" + info.getMembers().size();
+                } else if (identifier.equals("island_togglewarp")) {
+                    IslandInfo info = uSkyBlock.getAPI().getIslandInfo(p);
+                    return info == null ? "None" : (info.getWarpLocation() == null ? "Inactive" : "Active");
+                } else if (identifier.equals("island_lock")) {
+                    IslandInfo info = uSkyBlock.getAPI().getIslandInfo(p);
+                    return info == null ? "None" : isLocked(info) ? "Locked" : "Unlocked";
                 }
                 return null;
             }
 
         }.hook();
+    }
+
+    public static boolean isLocked(IslandInfo info) {
+        try {
+            if (IS_LOCKED == null) {
+                IS_LOCKED = info.getClass().getMethod("isLocked");
+            }
+            return Boolean.valueOf(IS_LOCKED.invoke(info).toString()).booleanValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
